f863c56a423987d238757447b0c132459d74fac0
unknown <franko122>
2025-09-20 07:56:45 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function StartCode (){
-    
-}
+    let button = document.getElementById("two");
+    let input = document.getElementById("one");
+    let code  = document.getElementById("code");
+    button.addEventListener("click", ()=>{
+        if (!input.value) { 
+            code.style.display="block";
+            setTimeout(() => {
+                 code.style.display="none";
+            }, 2000);
+        }
+    })         
+    }
 StartCode();
 
 
