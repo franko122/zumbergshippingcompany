7c5a1a668d3b78d8a4d1c934ee53c34938b18f25
unknown <franko122>
2025-09-21 09:15:10 -0700

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ function StartCode (){
     let button = document.getElementById("two");
     let input = document.getElementById("one");
     let code  = document.getElementById("code");
-
+    let  lide =document.getElementById("lide")
 
      let tracknumbe = 63452452454
      let tracktw =  884989486533
+lide.addEventListener("click", ()=>{
+     let het = 250   
+        drive.style.height="250px";
+    if (drive.style.height == het) { 
+        drive.style.height="0px"; 
+       } 
 
+})
     button.addEventListener("click", ()=>{
         if (input.value == tracknumbe ) { 
             window.location = "trackone.html";
