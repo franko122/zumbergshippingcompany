103f1e23c57f3b75b45dd3ad63f51fde6c8c5294
franko122 <83500527+franko122@users.noreply.github.com>
2025-09-22 19:01:33 +0100

    Update second.js

--- second.js
+++ second.js
@@ -18,6 +18,8 @@ function Secons() {
        }
        else{ 
             code.style.display="block"; 
+           code.innerText="Please insert a valid code "
+             code.style.color="red"
             setTimeout(() => {
                  code.style.display="none"; 
                   one.style.border=" rgb(0, 77, 165) solid 2px";
