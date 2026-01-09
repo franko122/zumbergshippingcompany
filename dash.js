47c72cd56150c74aeed4a19a9715623e519cbb24
franko122 <83500527+franko122@users.noreply.github.com>
2026-01-09 10:22:51 +0100

    Update dash.js

--- dash.js
+++ dash.js
@@ -3,9 +3,10 @@ function StartCode() {
          const dashin = document.getElementById("frank");
          const ballance = document.getElementById("balance");
          const refresh = document.getElementById("refresh");
+        
          refresh.addEventListener('click', () => {
 dashin.innerText="Darlene266"
        });
-         
+         alert(JSON.parse(localStorage.getItem("users")))
 }
 StartCode();
