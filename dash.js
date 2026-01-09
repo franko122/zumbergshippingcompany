8648ed9a75ce059ce61f974685ee1b1c89687265
franko122 <83500527+franko122@users.noreply.github.com>
2026-01-09 10:14:59 +0100

    Update dash.js

--- dash.js
+++ dash.js
@@ -3,23 +3,9 @@ function StartCode() {
          const dashin = document.getElementById("frank");
          const ballance = document.getElementById("balance");
          const refresh = document.getElementById("refresh");
-         const franklin = 'franklin' 
-         const frank = "frank";
-         const sheikh = "Sheikh";    
-         if (dashin.innerHTML == sheikh) { 
-             earning.innerHTML="450";
-             ballance.innerHTML="50";
-            }
-            if (dashin.innerText == frank) {
-                earning.innerHTML="0"
-                ballance.innerHTML="0"
-            } 
-            if (dashin.innerText == franklin) {
-                earning.innerHTML="200"
-                ballance.innerHTML="500"    
-} 
          refresh.addEventListener('click', () => {
-           alert("hi")
+dashin.innerText="Darlene266"
        });
+         
 }
 StartCode();
